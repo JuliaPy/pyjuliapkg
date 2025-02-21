889f2f0a22b87be831c55e3f4884d9a02694259c
Leon Teichroeb <bigleon00@gmail.com>
2025-02-21 23:27:43 +0100

    Throw exception when `julia --version` fails. (#49)
    * Throw exception when `julia --version` fails.
    
    * Fix formatting
    
    * make error message consistent
    
    ---------
    
    Co-authored-by: Christopher Doris <github.com/cjdoris>

--- src/juliapkg/find_julia.py
+++ src/juliapkg/find_julia.py
@@ -201,5 +201,9 @@ def ju_find_julia_noinstall(compat=None):
         versions.sort(key=lambda x: x[1], reverse=True)
         for exe, _ in versions:
             ver = julia_version(exe)
+            if ver is None:
+                raise Exception(
+                    f"{exe} (installed by juliaup) is not a valid Julia executable"
+                )
             if compat is None or ver in compat:
                 return (exe, ver)
